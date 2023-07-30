6a7720f940b01cf3e7c98965f0ac1402e446275a
jafarova1 <gultajvj@code.edu.az>
2023-07-31 01:18:19 +0400

    added update pagination

--- js/paginate.js
+++ js/paginate.js
@@ -1,9 +1,47 @@
-var pagination = document.querySelector('.pagination');
-var pages = document.querySelectorAll('.pagination li');
-for (var i = 0; i < pages.length; i++) {
-  pages[i].addEventListener('click', function() {
-    var activePage = document.querySelector('.pagination .active');
-    activePage.classList.remove('active');
-    this.classList.add('active');
-  });
+// var pagination = document.querySelector('.pagination');
+// var pages = document.querySelectorAll('.pagination li');
+// for (var i = 0; i < pages.length; i++) {
+//   pages[i].addEventListener('click', function() {
+//     var activePage = document.querySelector('.pagination .active');
+//     activePage.classList.remove('active');
+//     this.classList.add('active');
+//   });
+// }
+
+
+let link=document.getElementsByClassName("link");
+let currentValue=1;
+
+function activeLink(){
+  for(l of link){
+      l.classList.remove("active");
+  }
+  event.target.classList.add("active");
+  currentValue=event.target.value;
+
+
 }
+
+function backBtn(){
+  if(currentValue>1){
+    for(l of link){
+      l.classList.remove("active");
+  }
+  currentValue--;
+  link[currentValue-1].classList.add("active")
+  }
+}
+
+function nextBtn(){
+  if(currentValue<6){
+    for(l of link){
+      l.classList.remove("active");
+  }
+  currentValue++;
+  link[currentValue-1].classList.add("active")
+  }
+}
+
+
+
+
